3dc62f6141fd1afcf3ab588dcddd3e8ba673a8c8
tugukurechan <tugukurechan@gmail.com>
2024-06-10 17:36:10 +0900

    細かい修正

--- src/main/java/com/example/repository/AdministratorRepository.java
+++ src/main/java/com/example/repository/AdministratorRepository.java
@@ -9,7 +9,6 @@ import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.example.domain.Administrator;
 
@@ -89,7 +88,7 @@ public class AdministratorRepository {
 		String sql = "select id,name,mail_address,password from administrators where mail_address=:mailAddress";
 		SqlParameterSource param = new MapSqlParameterSource().addValue("mailAddress", mailAddress);
 		List<Administrator> administratorList = template.query(sql, param, ADMINISTRATOR_ROW_MAPPER);
-		if (administratorList.size() == 0) {
+		if (administratorList.isEmpty()) {
 			return null;
 		}
 		return administratorList.get(0);
